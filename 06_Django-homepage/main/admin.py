e405f70f5b955aa6f84bb667386f76f71618288e
pythonlessons <pythonlessons0@gmail.com>
2022-05-20 11:34:55 +0300

    Tutorial 6 files

---
+++ 06_Django-homepage/main/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Article, ArticleSeries
+
+class ArticleSeriesAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'subtitle',
+        'slug',
+        # 'published'
+    ]
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Header", {"fields": ['title', 'subtitle', 'article_slug', 'series']}),
+        ("Content", {"fields": ['content', 'notes']}),
+        ("Date", {"fields": ['modified']})
+    ]
+
+# Register your models here.
+admin.site.register(ArticleSeries, ArticleSeriesAdmin)
+admin.site.register(Article, ArticleAdmin)

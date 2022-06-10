9112d0462e6854db291de470e5b71a2d92f0b498
Rokas Balsys <rokasbalsys92@gmail.com>
2022-06-10 13:52:56 +0300

    Tutorial part 9 - login and logout functions

--- 09_Django-login-logout/users/urls.py
+++ 09_Django-login-logout/users/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path("register", views.register, name="register"),
+    path('login', views.custom_login, name='login'),
+    path('logout', views.custom_logout, name='logout'),
 ]
--- 09_Django-login-logout/users/views.py
+++ 09_Django-login-logout/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model, login
+from django.contrib.auth import get_user_model, login, logout, authenticate
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def register(request):
@@ -25,6 +27,41 @@ def register(request):
 
     return render(
         request=request,
-        template_name = "users/register.html",
+        template_name="users/register.html",
         context={"form": form}
         )
+
+
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('homepage')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Hello <b>{user.username}</b>! You have been logged in")
+                return redirect('homepage')
+
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error) 
+
+    form = AuthenticationForm() 
+    
+    return render(
+        request=request,
+        template_name="users/login.html", 
+        context={'form': form}
+        )
+
+@login_required
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("homepage")

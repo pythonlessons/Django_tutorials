c0c1d9e2824b2d9318ccb5db77323306a94d105f
Rokas Balsys <rokasbalsys92@gmail.com>
2022-06-14 10:28:07 +0300

    Tutorial part 9 - login and logout functions

--- 09_Django-login-logout/users/views.py
+++ 09_Django-login-logout/users/views.py
@@ -5,6 +5,7 @@ from .forms import UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 
+
 # Create your views here.
 def register(request):
     if request.user.is_authenticated:
@@ -31,37 +32,36 @@ def register(request):
         context={"form": form}
         )
 
-
-def custom_login(request):
-    if request.user.is_authenticated:
-        return redirect('homepage')
-
-    if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                messages.success(request, f"Hello <b>{user.username}</b>! You have been logged in")
-                return redirect('homepage')
-
-        else:
-            for error in list(form.errors.values()):
-                messages.error(request, error) 
-
-    form = AuthenticationForm() 
-    
-    return render(
-        request=request,
-        template_name="users/login.html", 
-        context={'form': form}
-        )
-
 @login_required
 def custom_logout(request):
     logout(request)
     messages.info(request, "Logged out successfully!")
     return redirect("homepage")
+
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect("homepage")
+
+    if request.method == "POST":
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data["username"],
+                password=form.cleaned_data["password"],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Hello <b>{user.username}</b>! You have been logged in")
+                return redirect("homepage")
+
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error) 
+
+    form = AuthenticationForm()
+
+    return render(
+        request=request,
+        template_name="users/login.html",
+        context={"form": form}
+        )

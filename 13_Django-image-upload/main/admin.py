acc7b4273aeafb434a681b99bc3b88af1844a1df
Rokas Balsys <rokasbalsys92@gmail.com>
2022-06-21 14:59:32 +0300

    Initial 13th tutorial - image upload

---
+++ 13_Django-image-upload/main/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Article, ArticleSeries
+
+class ArticleSeriesAdmin(admin.ModelAdmin):
+    fields = [
+        'title',
+        'subtitle',
+        'slug',
+        'author',
+        # 'published'
+    ]
+
+class ArticleAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Header", {"fields": ['title', 'subtitle', 'article_slug', 'series', 'author']}),
+        ("Content", {"fields": ['content', 'notes']}),
+        ("Date", {"fields": ['modified']})
+    ]
+
+# Register your models here.
+admin.site.register(ArticleSeries, ArticleSeriesAdmin)
+admin.site.register(Article, ArticleAdmin)

94b4d733a26446d09c1c277b3cb9ffdfc55a4648
Rokas Balsys <rokasbalsys92@gmail.com>
2022-06-21 09:14:20 +0300

    creating 12th tutorial part

---
+++ 12_Django-user-profile/users/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+# from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path("register", views.register, name="register"),
+    path('login', views.custom_login, name='login'),
+    path('logout', views.custom_logout, name='logout'),
+    # path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    # path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+]

c4f3d3f5527e9793c6d76eeee173b327b74baa15
Rokas Balsys <rokasbalsys92@gmail.com>
2022-06-23 09:32:55 +0300

    Initial 14th tutorial - page managing

---
+++ 14_Django-page-managing/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("register", views.register, name="register"),
+    path('login', views.custom_login, name='login'),
+    path('logout', views.custom_logout, name='logout'),
+    path('profile/<username>', views.profile, name='profile'),
+]

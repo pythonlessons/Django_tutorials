0e2bb4de451d6c1c42c0e5ecfae680a4a8d1e91a
Rokas Balsys <rokasbalsys92@gmail.com>
2022-07-11 16:24:28 +0300

    Initial 18th tutorial - Django subscription

---
+++ 18_Django-subscription/users/backends.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+from django.db.models import Q
+
+UserModel = get_user_model()
+
+class EmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserModel.objects.get(Q(username__iexact=username) | Q(email__iexact=username))
+        except UserModel.DoesNotExist:
+            UserModel().set_password(password)
+            return
+        except UserModel.MultipleObjectsReturned:
+            user = UserModel.objects.filter(Q(username__iexact=username) | Q(email__iexact=username)).order_by('id').first()
+
+        if user.check_password(password) and self.user_can_authenticate(user):
+            return user

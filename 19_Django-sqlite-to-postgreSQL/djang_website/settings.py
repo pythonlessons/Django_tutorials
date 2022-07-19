7fc45865c89b2af706f1f3c816e868ff83a2df59
Rokas Balsys <rokasbalsys92@gmail.com>
2022-07-19 16:19:28 +0300

    Tutorial part 19 - SQLite to PostgreSQL migration

--- 19_Django-sqlite-to-postgreSQL/djang_website/settings.py
+++ 19_Django-sqlite-to-postgreSQL/djang_website/settings.py
@@ -83,13 +83,23 @@ WSGI_APPLICATION = 'djang_website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '45.76.42.234',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
